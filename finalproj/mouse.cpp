937df1f02befe6f33962469d859045981688fc3e
ktrinanm <ktrinanl@gmail.com>
2017-04-10 13:05:06 -0600

    Adding final projects and some homework

---
+++ finalproj/mouse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <libusb.h>
+
+using namespace std;
+
+int main()
+{
+	libusb_device **devs; //pointer to pointer of device
+	libusb_context *context = NULL; //a libusb session
+
+	return 1;
+}
